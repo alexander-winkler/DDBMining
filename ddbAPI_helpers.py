ca4bea49d0262e6d3e78f80f98fae25b095c6251
alexander-winkler <alexander_winkler@posteo.de>
2022-08-04 22:42:46 +0200

    minor changes

--- ddbAPI_helpers.py
+++ ddbAPI_helpers.py
@@ -44,12 +44,13 @@ def infoID(ID, api_key):
     return res
 
 
-def iterateAPICall(apiurl:str, api_key, download = False, sourcexml = True, targetdir = None):
+def iterateAPICall(apiurl:str, api_key, download = False, sourcexml = True, targetdir = None) -> list:
     '''
     iterativer API-Call in 100er-Schritten. Möglichkeit des Download (bei download == True)
     Wenn sourcexml == True wird aus der Antwort die Quell-XML extrahiert und heruntergeladen,
     sonst die gesamte JSON-Antwort.
     Es ist möglich, mit targetdir einen Pfad zu definieren, in dem die Dateien gespeichert werden.
+    Gibt eine Liste mit den Objekt-JSON-Dateien zurück.
     '''
     
     print("API-Call\t" + apiurl)
@@ -61,7 +62,7 @@ def iterateAPICall(apiurl:str, api_key, download = False, sourcexml = True, targ
     
     hits = hitsAPICall(apiurl)
     output = []
-    for i in range(1,hits +1 , 100):
+    for i in range(0,hits , 100):
         print(f"{i} bis {i+100} (von {hits})")
         res = requests.get(apiurl + f"&rows=100&offset={i}").json().get('results')[0].get('docs')
         output.extend(res)
@@ -80,6 +81,7 @@ def iterateAPICall(apiurl:str, api_key, download = False, sourcexml = True, targ
                 except Exception as e:
                     print(e)
                     print(infoID(i.get('id'), api_key).url)
+    return output
     
 def datasetFromObject(object_ID:str, api_key) -> str:
     '''
@@ -90,3 +92,46 @@ def datasetFromObject(object_ID:str, api_key) -> str:
     print(f"Dataset-ID zu Objekt {object_ID} lautet: {dataset_ID}")
     apiurl = f"https://api.deutsche-digitale-bibliothek.de/search?query=dataset_id%3A%28{dataset_ID}%29&oauth_consumer_key={api_key}"
     return apiurl
+
+def thumbFlipbook(objectlist: list, filename = "ddb_art.gif",  img_number = 10, duration = 100):
+    '''
+    Macht aus dem Output von iterateAPICall eine Gif-Datei.
+    Beschränkung der Bilderzahl auf img_number.
+    Wenn img_number = None, dann werden alle Bilder in eine Gif konvertiert.
+    Dateiname der Gif kann mit "filename" verändert werden.
+    Funktioniert aktuell nur bei gleichgroßen Bildern.
+    '''
+    from PIL import Image
+    from io import BytesIO
+    from time import sleep
+
+    if img_number <= len(objectlist):
+        n = img_number
+    else:
+        n = len(objectlist)
+    
+    thumbnails = []
+
+    for x in objectlist[:n]:
+        try:
+            thumbnail_uuid = x.get('thumbnail')
+            res = requests.get(f"https://iiif.deutsche-digitale-bibliothek.de/image/2/{thumbnail_uuid}/full/!116,87/0/default.jpg")
+            img = Image.open(BytesIO(res.content))
+            thumbnails.append(img)
+            sleep(1)
+        except Exception as e:
+            print(e)
+    
+    if len(thumbnails) > 0:
+        frame_one = thumbnails[0]
+        frame_one.save(
+            filename,
+            format="GIF",
+            append_images=thumbnails,
+            save_all=True,
+            duration=duration,
+            loop=0
+            )
+    else:
+        print("Keine Thumbnails vorhanden.")
+        
